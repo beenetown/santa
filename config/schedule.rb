c5b8d16917f738f548fe263851a2a4bd52f216fe
John Beene <JohnBeeneNYC@gmail.com>
2014-09-15 15:41:39 -0400

    changed schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
-# set :output, "#{path}/log/cron.log"
-# set :environment, :production
+set :output, "#{path}/log/cron.log"
+set :environment, :production
 
-# every 1.hours do
-#   runner "Gift.pull_from_hat"
-# end
+every 1.minutes do
+  runner "Gift.pull_from_hat"
+end
 
 # every 1.days do
 #   runner "User.destroy_old_guests"
